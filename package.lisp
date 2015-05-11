1976cb8ac72916d9358fed24ee24c16673faa7f3
Shinmera <shinmera@tymoon.eu>
2015-05-11 16:06:00 +0200

    exports

--- package.lisp
+++ package.lisp
@@ -8,5 +8,70 @@
 (defpackage #:abcd
   (:nicknames #:org.shirakumo.abcd)
   (:use #:cl)
+  ;; compilers.lisp
   (:export
-   ))
+   #:c-compiler
+   #:executable
+   #:invoke
+   #:*default-compiler*
+   #:c-preprocess
+   #:c-assemble
+   #:c-link
+   #:c-compile
+   #:clang
+   #:gcc
+   #:ensure-compiler)
+  ;; components.lisp
+  (:export
+   #:flag-component
+   #:component-direct-flags
+   #:component-effective-flags
+   
+   #:multi-type-source-file
+   #:file-types
+   
+   #:c-file
+   #:c-header
+   #:c++-file
+   #:c++-header
+   #:program
+   #:make)
+  ;; operations.lisp
+  (:export
+   #:*default-flags*
+   #:c-compiler-op
+   #:operation-direct-flags
+   #:operation-effective-flags
+   #:operation-compiler-function
+   #:operation-compiler
+   #:execute
+   
+   #:compute-flags-op
+   #:preprocess-op
+   #:assemble-op
+   #:link-op)
+  ;; systems.lisp
+  (:export
+   #:c-system
+   #:default-header-class
+   #:c-system-compiler
+   #:c-system-output
+   #:c-system-shared-library
+   
+   #:c++-system
+   
+   #:*standard-asdf-class-for-type*
+   #:*default-header-class*
+   
+   #:preprocess-system
+   #:assemble-system
+   #:link-system)
+  ;; toolkit.lisp
+  (:export
+   #:externalize
+   #:shellify
+   #:with-cleaned-files
+   #:merge-flags
+   #:component-path
+   #:minimal-shell-namestring
+   #:version<))
